322700a5d6a1928a59f4c0f4e247a27fee39e8d8
Andrew Shaffer <andrew@weo.io>
2013-12-20 12:11:23 -0800

    first draft

---
+++ index.js
@@ -0,0 +1,19 @@
+var through = require('through')
+  , Minimatch = require('minimatch').Minimatch;
+
+
+module.exports = function(map) {
+  var patterns = Object.keys(map)
+    , minimatchers = patterns.map(function(pat, idx) {
+      return new Minimatch(pat).makeRe();
+    });
+
+  return function(file) {
+    for(var i = 0; i < minimatchers.length; i++) {
+      if(minimatchers[i].test(file)) {
+        return fs.createReadStream(map[patterns[i]]);
+      }
+    }
+    return through();
+  }
+};
